b03eae055bea9cc6705349256d3459a4c938275a
Bryan Rayner <bryanerayner@gmail.com>
2017-03-06 21:05:11 -0600

    Add support for $expires, and add has() method

--- src/json-graph/json-graph.spec.ts
+++ src/json-graph/json-graph.spec.ts
@@ -5,15 +5,36 @@ import {
     expect
 } from 'chai';
 
+import sinon = require('sinon');
+
 
 describe('JsonGraph', ()=>{
 
     describe('set', ()=>{
 
+        it('should work with POJOs', ()=>{
+            let g = new JsonGraph();
+            const value = {
+                something: {
+                    cool: ['is here!']
+                },
+                anythingElse: [
+                    {
+                        value: 'You\'d like to mention?'
+                    }
+                ]
+            };
+            g.set([], value);
+
+            let val = g.getSync([]);
+
+            expect(val).to.deep.equal(value);
+
+        })
         
         it('should work with ref sentinel values', ()=>{
 
-            let g = new JsonGraph();
+            let g = new JsonGraph('');
 
             g.set(['dataById'], {
                 '1': 'two',
@@ -52,7 +73,7 @@ describe('JsonGraph', ()=>{
         
         it('should allow values to be set on the root.', ()=>{
 
-            let g = new JsonGraph();
+            let g = new JsonGraph('');
 
             g.set([], {
                 foo: {
@@ -70,7 +91,7 @@ describe('JsonGraph', ()=>{
         
         it('should allow deep recursion of references.', ()=>{
 
-            let g = new JsonGraph();
+            let g = new JsonGraph('');
 
             g.set([], {
                 foo: {
@@ -97,7 +118,7 @@ describe('JsonGraph', ()=>{
         
         it('should work with ref sentinel values', ()=>{
 
-            let g = new JsonGraph();
+            let g = new JsonGraph('');
 
             g.set([], {
                 dataById: {
@@ -124,7 +145,139 @@ describe('JsonGraph', ()=>{
             expect(bees).to.equal('bees');
         });
 
+        it('should allow the definition of an "undefined" value', ()=>{
 
+            let g = new JsonGraph();
+
+            g.set([1], 'four');
+
+            let five = g.getSync([2], 'five');
+            expect(five).to.equal('five');
+        });
+
+        it('should retrieve the undefined value when expired references have been traversed', ()=>{
+
+            let g = new JsonGraph('');
+
+            sinon.stub(g, 'dateNow', function() {
+                return 2000;
+            });
+            let dateNow = g.dateNow();
+            expect(dateNow).to.equal(2000);
+
+            g.set([], {
+                dataById: {
+                    '1': 'two',
+                    '3': {
+                        'apple': 'bees',
+                        'knees': 'sneeze'
+                    }
+                },
+                users: {
+                    4: ref(['dataById', '3'], {$expires: 1000}),
+                    5: ref(['dataById', '3'], {$expires: 3200})
+                }
+            });
+
+            let nope = g.getSync(['users', 4], 'nope');
+
+            expect(nope).to.equal('nope');
+
+            let niceTry = g.getSync(['users', 4, 'apple'], 'niceTry');
+            expect(niceTry).to.equal('niceTry');
+
+            let bees = g.getSync(['users', 5, 'apple']);
+            expect(bees).to.equal('bees');
+        });
+
+        it('should unwrap atoms', ()=>{
+
+            let g = new JsonGraph();
+
+            g.set([1], {
+                $type: 'atom',
+                value: 'teh'
+            });
+
+            let teh = g.getSync([1]);
+            expect(teh).to.equal('teh');
+        });
+
+        it('should not unwrap expired atoms', ()=>{
+
+            let g = new JsonGraph();
+
+            g.set([1], {
+                $type: 'atom',
+                $expires: 0,
+                value: 'teh'
+            });
+
+            let teh = g.getSync([1]);
+            expect(teh).to.equal(undefined);
+        });
+
+        it('should not unwrap expired atoms with a non-zero timestamp', ()=>{
+
+            let g = new JsonGraph();
+
+            g.set([1], {
+                $type: 'atom',
+                $expires: 1000,
+                value: 'teh'
+            });
+
+            sinon.stub(g, 'dateNow', function() {
+                return 2000;
+            });
+            let dateNow = g.dateNow();
+            expect(dateNow).to.equal(2000);
+
+            let teh = g.getSync([1]);
+            expect(teh).to.equal(undefined);
+        });
+
+
+    });
+
+    describe('has', ()=>{
+
+        it('should return true if a path exists', ()=>{
+
+            let g = new JsonGraph('');
+
+            g.set([], {
+                dataById: {
+                    '1': 'two',
+                    '3': {
+                        'apple': 'bees',
+                        'knees': 'sneeze'
+                    }
+                },
+                users: {
+                    4: {
+                        data: ref(['dataById', '3'])
+                    }
+                }
+            });
+
+            let exists = g.has(['users', 4, 'data']);
+            expect(exists).to.equal(true);
+
+        });
+
+        it('should return false if a path does not exist', ()=>{
+
+
+            let g = new JsonGraph('');
+
+            g.set([], {
+                cool: 'beans!'
+            });
+
+            let exists = g.has(['derpyderpydoo']);
+            expect(exists).to.equal(false);
+        });
     });
 
 });
